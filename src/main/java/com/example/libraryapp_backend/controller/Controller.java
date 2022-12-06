91cad2ee5f2c9fd95ccd005813792fdc117072d3
Varun-Babu <itsme.varun.26@gmail.com>
2022-12-06 07:26:41 +0530

    home

--- src/main/java/com/example/libraryapp_backend/controller/Controller.java
+++ src/main/java/com/example/libraryapp_backend/controller/Controller.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
+    @PostMapping("/")
+    public String Home(){
+        return "home";
+    }
+
     @PostMapping("/add")
     public String Add(){
         return "add";
